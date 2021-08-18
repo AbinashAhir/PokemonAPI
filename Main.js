2a05c784a12f1891cfaf5ed9aa1e5768b6e833fb
Abinash Ahir <ahirabinash@gmail.com>
2021-08-18 20:21:32 +0530

    initial commit

---
+++ Main.js
@@ -0,0 +1,32 @@
+function myFunction(){
+  var a= document.getElementById("name1").value;
+
+
+const apiData = {
+    url: "https://pokeapi.co/api/v2/",
+    type: 'pokemon',
+    id: a,
+}
+const {url,type,id} = apiData
+
+const apiUrl = `${url}${type}/${id}`
+
+fetch(apiUrl)
+.then((data)=> data.json())
+.then((pokemon) => genetrateHtml(pokemon))
+
+const genetrateHtml = (data) => {
+    console.log(data);
+    const html = `
+    <div class="name">${data.name}</div>
+    <img src = ${data.sprites.front_default}>
+    <div class="details">
+        <span>Height: ${data.height}</span>
+        <span>Weight: ${data.weight}</span>
+    </div>
+    `
+    const pokemonDiv = document.querySelector('.pokemon')
+    pokemonDiv.innerHTML = html
+}
+
+}
